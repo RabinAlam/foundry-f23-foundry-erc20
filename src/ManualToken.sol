64d83ba80a46b06e6a03452ae266bffd07285085
sumon <sumon255sarkar@gmail.com>
2025-11-29 16:36:51 +0600

    Format Solidity code with forge fmt

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -1,29 +1,42 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.18;
 
-contract ManualToken{    
-   mapping (address => uint256) private s_balances;
-   
- function name() public pure returns(string memory){
-    return "Manual Token";
+contract ManualToken {
+    mapping(address => uint256) private s_balances;
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    constructor() {
+        s_balances[msg.sender] = totalSupply();
+        emit Transfer(address(0), msg.sender, totalSupply());
     }
 
- function totalSupply() public pure returns (uint256){
-    return 100 ether;
-   }
+    function name() public pure returns (string memory) {
+        return "Manual Token";
+    }
 
-  function decimals() public pure returns (uint8){
-    return 18 ;
-   }
+    function totalSupply() public pure returns (uint256) {
+        return 100 ether;
+    }
 
-   function balanceOf(address _owner) public view returns (uint256){
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256) {
         return s_balances[_owner];
-        }
+    }
 
     function transfer(address _to, uint256 _amount) public {
-    uint256 previousBalance = balanceOf(msg.sender) + balanceOf(_to);
-    s_balances[msg.sender] -= _amount;
-    s_balances[_to] += _amount;
-    require(balanceOf(msg.sender) + balanceOf(_to) == previousBalance);
-     }
+        require(_to != address(0), "Transfer to zero address");
+        require(_amount > 0, "Transfer amount must be positive");
+        require(s_balances[msg.sender] >= _amount, "Insufficient balance");
+        
+        if (msg.sender != _to) {
+            s_balances[msg.sender] -= _amount;
+            s_balances[_to] += _amount;
+        }
+        
+        emit Transfer(msg.sender, _to, _amount);
+    }
 }
